a01e9f2cc0e964740e1048af563c296cf0fd5074
Nas <anaszackariah73@gmail.com>
2026-02-12 18:45:28 +0000

    Fix formatting of operation input prompt

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -3,7 +3,7 @@
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))    
 #prompt for the type of opeartion
-operation = input("Choose the operation (+ , - , * , /):. ")
+operation = input("Choose the operation (+, -, *, /):. ")
 #Implement a Match Case statement that executes the chosen operation based on the user’s inpu
 match operation:
     case "+":
