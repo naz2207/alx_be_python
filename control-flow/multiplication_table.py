937eab13b7ad16f5aa8a4d43eb64f8c0b6843fc1
Nas <anaszackariah73@gmail.com>
2026-02-13 04:30:29 +0000

    Update multiplication_table.py

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,6 +1,6 @@
 #prompt the user for the number1 and 2
 # Prompt user for a number
-number = int(input("Enter a number to see its multiplication table:"))
+number = int(input("Enter a number to see its multiplication table: "))
 
 # Generate multiplication table from 1 to 10
 for i in range(1, 11):
